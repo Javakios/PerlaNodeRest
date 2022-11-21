3a4ceae249185ca6b27651652284309a25c69875
Javakios <dinos18kostas@gmail.com>
2022-11-21 21:41:59 +0200

    Related Products Route/Controller

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,6 @@ const database = require('../db');
 
 // Get All Products 
 exports.getProducts = (req,res,next)=>{
-
     database.execute("SELECT * FROM products")
     .then(async products =>{
         hasOffer = false;
@@ -12,7 +11,7 @@ exports.getProducts = (req,res,next)=>{
         let homeOffer = false;
         for(let i = 0 ; i < products[0].length;i++){
             if(products[0][i].p_wholesale_price !=products[0][i].p_offer ) hasOffer = true;
-            console.log(`SELECT * FROM products_offer WHERE product_mtrl= ${products[0][i].p_mtrl}`);
+            else hasOffer = false;
             let homePageOffer =await  database.execute(`SELECT * FROM products_offer WHERE product_mtrl= ${products[0][i].p_mtrl}`)
             let images =await database.execute(`SELECT * FROM product_images WHERE p_mtrl=${products[0][i].p_mtrl}`)
             let otherImages = [];
@@ -53,6 +52,7 @@ exports.getProducts = (req,res,next)=>{
                 'kodikos_kataskeuasti':products[0][i].p_code_kataskeuasti,
                 'texnikos_kodikos':products[0][i].p_code_texniko,
         }} 
+        database.end()
         res.status(200).json({
             message:"All Products",
             products: returnProds
@@ -61,4 +61,73 @@ exports.getProducts = (req,res,next)=>{
         if(!err.statusCode) err.statusCode = 500
         next(err);
     })
+}
+
+// Get All Products Related
+exports.getProductsRelated = async(req,res,next)=>{
+    let returnProds = [];
+    let bad = false;
+    if(!req.body.mtrl){
+        bad = true;
+    }else{
+
+        let mtrl = req.body.mtrl;
+        console.log(`SELECT p_related_mtrl FROM product_page_related WHERE p_mtrl=${mtrl}`);
+        let related_mtrl = await  database.execute(`SELECT p_related_mtrl FROM product_page_related WHERE p_mtrl= ${mtrl} `);
+        console.log(related_mtrl[0][0].p_related_mtrl);
+        
+       
+        for(let i = 0 ; i < related_mtrl[0].length;i++){
+            related_mtrl = related_mtrl[0][i].p_related_mtrl
+            let prod = await database.execute(`SELECT * FROM products where p_mtrl=${related_mtrl}`);
+            if(prod[0][0].p_wholesale_price !=prod[0][0].p_offer ) hasOffer = true;
+            else hasOffer = false;
+            let homePageOffer =await  database.execute(`SELECT * FROM products_offer WHERE product_mtrl= ${prod[0][0].p_mtrl}`)
+            let images =await database.execute(`SELECT * FROM product_images WHERE p_mtrl=${prod[0][0].p_mtrl}`)
+            let otherImages = [];
+            for(let i = 0 ; i < images[0].length;i++){
+                otherImages[i] = {
+                    'img': images[0][i].p_image
+                }
+            }
+            if(homePageOffer[0].length != 0) homeOffer = true;
+            else homeOffer = false;    
+            returnProds[i] = {
+                'mtrl' :prod[0][0].p_mtrl,
+                'omada':prod[0][0].p_omada,
+                'name' :prod[0][0].p_name,
+                'name1' :prod[0][0].p_name1,
+                'retailPrice' :prod[0][0].p_retail_price,
+                'wholesalePrice' :prod[0][0].p_wholesale_price,
+                'offer':prod[0][0].p_offer,
+                'hasOffer':hasOffer,
+                'homePageOffer': homeOffer,
+                'discount':prod[0][0].p_dicount,
+                'code' :prod[0][0].p_kod,
+                'description_eng':prod[0][0].p_desc_eng,
+                'stock' :prod[0][0].p_stock,
+                'diathesima':prod[0][0].p_diathesima,
+                'desmeumena':prod[0][0].p_desmeumena,
+                'category' :prod[0][0].p_category,
+                'subcategory' :prod[0][0].p_subcategory,
+                'image' :prod[0][0].p_image,
+                'otherImages' : otherImages ,
+                'description':prod[0][0].p_desc,
+                'pdf':prod[0][0].p_pdf,
+                'video':prod[0][0].p_yt_vid,
+                'data_sheet':prod[0][0].p_data_sheet,
+				'data_sheet_eng':prod[0][0].data_sheet_eng,
+                'onoma':prod[0][0].onoma_product,
+                'onoma_eng':prod[0][0].onoma_product_eng,
+                'kodikos_kataskeuasti':prod[0][0].p_code_kataskeuasti,
+                'texnikos_kodikos':prod[0][0].p_code_texniko,
+            }
+        }
+    }    
+    if(bad) {
+        res.status(404).json({message:"Fill The Required Fields"})
+    }else{
+        res.status(200).json({message:"Related Products",products:returnProds})
+    }
+
 }
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 // const {body} = require('express-')
 const router = express.Router();
 // controllers 
@@ -6,7 +7,7 @@ const productsController = require('../controllers/products');
 
 
 router.get('/getProducts',productsController.getProducts);
-
+router.post('/getAllproductsRelated',productsController.getProductsRelated)
 
 
 module.exports = router;
