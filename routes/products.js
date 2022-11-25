12c79f761a0924f6d0fca8e7793d86430ea3ff6d
NotMeAgain10 <dev@isupport.com.gr>
2022-11-25 13:58:27 +0200

    hello

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,7 @@ const { body } = require('express-validator/check');
 const router = express.Router();
 // controllers 
 const productsController = require('../controllers/products');
-
+router.post('/addToCart',productsController.addToCart);
 router.post('/getAllOffers',productsController.getOffers);
 router.get('/getProducts',productsController.getProducts);
 router.get('/getAllproductsRelated',productsController.getProductsRelated);
@@ -15,5 +15,5 @@ router.get('/seeEarlier',productsController.seeEarlier);
 router.get('/findMosquiProduct',productsController.findMosquiProduct);
 router.get('/findRelatedProducts',productsController.findRelatedProducts);
 router.get('/removeCartItem',productsController.removeCartItem);
-router.post('/addToCart',productsController.addToCart);
+
 module.exports = router;
