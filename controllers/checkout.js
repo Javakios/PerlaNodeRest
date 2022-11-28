24e3f21e2563d11d4d658d3da125f0bcb6591dd9
NotMeAgain10 <dev@isupport.com.gr>
2022-11-28 15:14:39 +0200

    revolout checkout

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -8,6 +8,24 @@ exports.checkout = (req, res, next) => {
       message: "Fill The require Fields",
     });
   } else {
-    
+    axios.post(
+        "https://merchant.revolut.com/api/1.0/orders",{
+            amount:amount,
+            currency:currency
+        },
+        {
+            headers:{
+                Accept:"application/json",
+                Authorization:"Bearer sk_gUE7hEJn4W2ya1F1fhZ979TgzSMu9d2q3w6_7WjFRe2D-OxZBC0aHMEtLmgsLUnC"
+            }
+        }
+    ).then(resData=>{
+        res.status(200).json({data:resData.data})
+    }).catch(err=>{
+        if(!err.statusCode){
+            err.statusCode=500;
+        }
+        next(err);
+    })
 }
 };
