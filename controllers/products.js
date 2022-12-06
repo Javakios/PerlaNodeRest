d10728c276132599e76514097b0a2b58281ac2de
NotMeAgain10 <dev@isupport.com.gr>
2022-12-06 17:38:20 +0200

    olakala

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,78 @@ const database = require("../db");
 
 // Get All Products
 exports.getProducts = (req, res, next) => {
-  const sub_cat_id = req.body.query;
-  if(!sub_cat_id) res.status(402).json({message:"fill the required fields"});
-  else{
-
-
-  database
-    .execute("SELECT * FROM products where p_subcategory=? order by onoma_product ASC",[sub_cat_id])
+  const sub_cat_id = req.query.sub_cat_id;
+  if(sub_cat_id){
+    database
+    .execute("SELECT * FROM products where p_subcategory=? order by onoma_product  ASC",[sub_cat_id])
+    .then(async (products) => {
+      hasOffer = false;
+      let returnProds = [];
+      let homeOffer = false;
+      for (let i = 0; i < products[0].length; i++) {
+        if (products[0][i].p_wholesale_price != products[0][i].p_offer)
+          hasOffer = true;
+        else hasOffer = false;
+        let homePageOffer = await database.execute(
+          `SELECT * FROM products_offer WHERE product_mtrl= ${products[0][i].p_mtrl}`
+        );
+        let images = await database.execute(
+          `SELECT * FROM product_images WHERE p_mtrl=${products[0][i].p_mtrl}`
+        );
+        let otherImages = [];
+        for (let i = 0; i < images[0].length; i++) {
+          otherImages[i] = {
+            img: images[0][i].p_image,
+          };
+        }
+        if (homePageOffer[0].length != 0) homeOffer = true;
+        else homeOffer = false;
+        returnProds[i] = {
+          mtrl: products[0][i].p_mtrl,
+          omada: products[0][i].p_omada,
+          name: products[0][i].p_name,
+          name1: products[0][i].p_name1,
+          retail: products[0][i].p_retail_price.toFixed(2),
+          wholesale: products[0][i].p_wholesale_price.toFixed(2),
+          offer: products[0][i].p_offer.toFixed(2),
+          hasOffer: hasOffer,
+          homePageOffer: homeOffer,
+          discount: products[0][i].p_dicount,
+          code: products[0][i].p_kod,
+          description_eng: products[0][i].p_desc_eng,
+          stock: products[0][i].p_stock,
+          diathesima: products[0][i].p_diathesima,
+          desmeumena: products[0][i].p_desmeumena,
+          category: products[0][i].p_category,
+          subcategory: products[0][i].p_subcategory,
+          image: products[0][i].p_image,
+          otherImages: otherImages,
+          description: products[0][i].p_desc,
+          pdf: products[0][i].p_pdf,
+          video: products[0][i].p_yt_vid,
+          data_sheet: products[0][i].p_data_sheet,
+          data_sheet_eng: products[0][i].data_sheet_eng,
+          onoma: products[0][i].onoma_product,
+          onoma_eng: products[0][i].onoma_product_eng,
+          kodikos_kataskeuasti: products[0][i].p_code_kataskeuasti,
+          texnikos_kodikos: products[0][i].p_code_texniko,
+          sintomi_per : products[0][i].sintomi_per,
+          sintomi_per_eng :products[0][i].sintomi_per_eng
+        };
+      }
+      // // // database.end();
+      res.status(200).json({
+        message: "All Products",
+        products: returnProds,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) err.statusCode = 500;
+      next(err);
+    });
+  }else{
+    database
+    .execute("SELECT * FROM products order by onoma_product ASC")
     .then(async (products) => {
       hasOffer = false;
       let returnProds = [];
@@ -75,6 +140,7 @@ exports.getProducts = (req, res, next) => {
       next(err);
     });
   }
+ 
 };
 
 // Get All Products Related
